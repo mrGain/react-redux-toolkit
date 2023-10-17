e6bac9c1dfaee904bf03088c3b7fe797d543737e
Rakesh Gain <rakeshgain47681@gmail.com>
2023-10-17 21:18:15 +0530

    working with multiple slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice';
+import authReducer from './authSlice';
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
+        auth: authReducer
     },
 });
 
